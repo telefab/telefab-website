aee1d3dba53f735f9bca682e8b0f36e30b916858
Tristan Groléat <tristan@macbook-tristan.maisel.enst-bretagne.fr>
2012-06-10 22:25:19 +0200

    Added basic django application
    The django application telefab has been modified to use a
    local_settings.py file for server-specific settings.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ telefab-website
 ===============
 
 Site web du Téléfab
+
+Installation
+---------------
+
+* Configurer WordPress en copiant le fichier wordpress/wp-config-sample.php en wp-config.php
+* Configurer Django en copiant le fichier django/telefab/telefab/local\_settings.sample.py en local\_settings.py
+* Configurer le serveur pour utiliser PHP sur wordpress/ (URL : /)
+* Configurer le serveur pour utiliser django sur django/telefab (URL : /gestion)
---
+++ django/telefab/telefab/local_settings.sample.py
@@ -0,0 +1,22 @@
+# Django local settings (per-server)
+
+# Debug mode?
+DEBUG = True
+
+# Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'telefab-website',                      # Or path to database file if using sqlite3.
+        'USER': 'telefab-website',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Django apps root directory
+GLOBAL_ROOT = '/home/tristan/Developpement/Web/telefab-website/django/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'ob#3jske&amp;-p0%5h%!z=bywt*$sl3euh=*soix0vmzn798vbn_('
