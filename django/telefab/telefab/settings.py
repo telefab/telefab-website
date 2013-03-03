bdbbe3bfc577a95cf5c0c8a4828610fe3322dace
Tristan Groléat <twisterss@gmail.com>
2013-03-04 00:21:30 +0100

    Fixed the allowed hosts for the new version of Django

--- django/telefab/telefab/settings.py
+++ django/telefab/telefab/settings.py
@@ -44,6 +44,9 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# Allowed website hosts
+ALLOWED_HOSTS = ['*']
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = GLOBAL_ROOT + 'media/'
