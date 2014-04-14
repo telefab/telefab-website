646c097511fc6d92d4fd7c9e4979a37abc982dae
Tristan Groléat <twisterss@gmail.com>
2014-04-14 11:08:52 +0200

    Adapted the announcements to the RAK screen
    This screen seems to lie about its size…

--- django/telefab/main/static/main/js/announcements.js
+++ django/telefab/main/static/main/js/announcements.js
@@ -7,7 +7,7 @@
  	var body = $('body');
  	// Adapt the font size to the screen width
  	function adaptFontSize() {
-	 	var baseFontSize = (body.width() - 800) * (35 - 25) / (1366 - 800) + 25;
+	 	var baseFontSize = (body.width() - 800) * (35 - 30) / (1366 - 800) + 30;
 	 	body.css('fontSize', Math.floor(baseFontSize) + "px");
 	}
 	$(window).resize(adaptFontSize);
