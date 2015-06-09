1370b7dd22d7387fb480c98613f117680dcce865
amauryfou <amaury.fouville@hotmail.fr>
2015-06-09 21:34:08 +0200

    Problème mail de rappel 2

--- django/telefab/main/management/commands/check_loans.py
+++ django/telefab/main/management/commands/check_loans.py
@@ -8,6 +8,6 @@ class Command(BaseCommand):
     help = 'Checks the loans for late returns'
 
     def handle(self, *args, **options):
-        for loan in Loan.objects.all():
+        for loan in Loan.objects.filter(panier=0).all():
             if loan.is_late() and loan.borrower.email:
                 loan.send_reminder()
